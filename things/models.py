304525191cf8bb6878ca61e9b114130faa1c040c
Khushi Jaiswal <14khushijais@gmail.com>
2022-12-05 05:58:31 +0000

    clear and make new migrations and database

--- things/models.py
+++ things/models.py
@@ -2,16 +2,9 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Thing(models.Model):
-#name: a short string that identifies a thing
-#must be unique, must not be blank, and must consist of 30 characters or less.
-    name = models.CharField(max_length=30, unique=True, blank=False )
-#description: a slightly longer string that describes a thing.
-#need not be unique, may be blank, and must consist of 120 characters of less.
-    description = models.CharField(max_length=120, unique = False, blank=True, )
-#quantity: an integer that identifies the number of items of a thing we possess.
-#need not be unique, and must be an integer value between 0 and 100 (inclusive). quantity may be 0 and it may be 100, but not less than 0 and not more than 100.
+    name = models.CharField(max_length=30, unique=True, blank=False)
+    description = models.CharField(max_length=120, blank=True, )
     quantity = models.IntegerField(
-            unique = False,
             validators=
                 [MinValueValidator(0),
                 MaxValueValidator(100)]
